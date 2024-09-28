316260a54eb6559ce8d8d80bb7bf3e86548dcc0f
Parth <parth.nakum@talentsystems.com>
2024-09-28 11:17:57 +0530

    added radio button, dropdown and textarea

--- tests/form.spec.ts
+++ tests/form.spec.ts
@@ -134,4 +134,114 @@ test("create profile form", async ({ page }) => {
   await expect(usernameLabel).toHaveValue("One Piece");
 
   await expect(usernameContainerMinCharErrorLocator).not.toBeVisible();
+
+  // test radio button
+  /**
+   * 1. get notifyMeAboutSection
+   * 2. get radio buttons
+   * 3. check radio button (apply different conditions)
+   * 4.
+   */
+
+  const notifyMeAboutSection = formLocator.locator("div").filter({
+    has: page.getByText("Notify me about..."),
+    // has: page.locator("label").getByText("Notify me about..."),
+  });
+  await expect(notifyMeAboutSection).toBeVisible();
+
+  /*
+  ex: 2
+    const notifyMeAboutSection = radioField.locator("div").locator("label");
+    await expect(notifyMeAboutSection).toHaveCount(3);
+    */
+  const radioBtnLabel = notifyMeAboutSection
+    .locator("div")
+    .filter({ hasNotText: "Notify me about..." })
+    .filter({ has: page.getByLabel("All new messages") })
+    .filter({ has: page.getByLabel("Direct messages and mentions") });
+  await expect(radioBtnLabel).toBeVisible();
+
+  const radioBtnOptions = radioBtnLabel
+    .getByRole("radio")
+    .and(page.locator("button"));
+  await expect(radioBtnOptions).toHaveCount(3);
+
+  /*
+  ex: 2
+  const radioBtnOptions = notifyMeAboutSection
+    .getByRole("radio")
+    .and(page.locator("button"));
+  await expect(radioBtnOptions).toHaveCount(3);
+   */
+
+  const firstRadioBtn = radioBtnOptions.first();
+  await expect(firstRadioBtn).toBeVisible();
+  await firstRadioBtn.click();
+  await expect(firstBtnOption).toBeChecked();
+
+  const secondRadioBtn = radioBtnOptions.nth(1);
+  await expect(secondRadioBtn).toBeVisible();
+  await expect(secondRadioBtn).not.toBeChecked();
+
+  const thirdRadioBtn = radioBtnOptions.last();
+  await expect(thirdRadioBtn).toBeVisible();
+  await expect(thirdRadioBtn).not.toBeChecked();
+  await thirdRadioBtn.click();
+  await expect(thirdRadioBtn).toBeChecked();
+  await expect(firstRadioBtn).not.toBeChecked();
+  await expect(secondRadioBtn).not.toBeChecked();
+
+  /* test dropdown button */
+
+  /*
+  const countrySection = formLocator
+    .locator("div")
+    .filter({ hasText: "Country" });
+  await expect(countrySection).toBeVisible();
+
+  const labelText = countrySection.getByText(
+    "This is the country you are currently living in."
+  );
+  await expect(labelText).toBeVisible();
+
+  const selectBtn = countrySection.locator("button");
+  await expect(selectBtn).toBeVisible();
+
+  const selectText = selectBtn.locator("span");
+  // const selectText = selectBtn.getByText("Select a verified email to display");
+
+  await expect(selectText).toBeVisible();
+  await expect(selectText).toHaveText("Select a verified email to display");
+
+  await selectBtn.click();
+
+  const selectOptions = countrySection.locator("select");
+  await expect(selectOptions).toBeVisible();
+  await selectOptions.selectOption("India");
+
+  const option = await selectOptions.inputValue();
+  // console.log(option);
+
+  await expect(selectOptions).toHaveValue(option);
+  await expect(selectText).toHaveText("India");
+
+  */
+
+  /* test bio text area*/
+
+  const bioSection = formLocator
+    .locator("div")
+    .filter({ hasText: "Bio" })
+    .filter({ hasNotText: "Country" });
+  await expect(bioSection).toBeVisible();
+
+  const textArea = bioSection.getByLabel("Bio");
+  await expect(textArea).toBeVisible();
+
+  const bioDescription = bioSection.getByText(
+    "You can @mention other users and organizations."
+  );
+  await expect(bioDescription).toBeVisible();
+
+  textArea.pressSequentially("Hii, there");
 });
